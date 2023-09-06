52127a0a3a6c033f764ad44153bf1f2d471760b1
Yannis <yannis182@outlook.fr>
2023-09-06 13:54:06 +0200

    chore: ugrade to Astro v3

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  darkMode: "class",
+};
